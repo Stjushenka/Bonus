e8d00d34ce5c7dd09676489b2642ea3597d3d058
Stjushenka <stjusheka@gmail.com>
2021-11-29 18:58:43 +0300

    Add bonus

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance1 = 100;
+        int replenishment = 100;
+        int bonus = replenishment/100;
+        if (replenishment > 100);
+        else {
+            bonus = 0;
+        }
+        int balance2 = balance1+replenishment+bonus;
+            System.out.println("Ваш баланс " +balance2);
+    }
+}
