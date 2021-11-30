41ce1204078a5187251b391bb7e16cd25d2d536b
Stjushenka <stjusheka@gmail.com>
2021-11-30 14:05:23 +0300

    Bonus

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int balance1 = 100;
         int replenishment = 100;
-        int bonus = replenishment/100;
-        if (replenishment > 100);
-        else {
+        int bonus;
+        if (replenishment > 100) {
+            bonus = replenishment/100;
+        } else {
             bonus = 0;
         }
         int balance2 = balance1+replenishment+bonus;
